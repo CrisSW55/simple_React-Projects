da09e0437905df6c36e64e0374da7ad70ff1dc84
Crystian Guzman <crystianguzman55@gmail.com>
2024-08-05 07:40:21 -0700

    fixed the second render of useEffect() hook which was caused by the <StrictMode> element over the <App> element/component in the index.js file inside the root.render(), also updated the App.js file to have two buttons, one adds to the useState 'myData',while the other button adds fetchedData from a url, to the useState 'todosData', after which each one will be mapped and displayed on screen or current state of the project/website

--- simple_reactjs-fetchdata/src/App.js
+++ simple_reactjs-fetchdata/src/App.js
@@ -1,23 +1,23 @@
-import './App.css'; 
 import { useState, useRef, useEffect } from 'react';
 
 const url = "https://jsonplaceholder.typicode.com/todos"
 
+function App() {
+  const [mydata,setMyData] = useState(null);
+  const [todosdata,setTodosData] = useState(null);
+  const [isLoading,setIsLoading] = useState(true);
 
-export default function App() {
-  const [data,setData] = useState([]);
 
+  const fetchData = async () => {
+    const fetched_Data = await fetch(url);
+    const json = await fetched_Data.json();
+    setTodosData(json);
+    setIsLoading(false);
+    console.log("Fetched data!"); 
+    console.log(json);
+  }
   useEffect(()=>{
-    const fetchData = async () => {
-      const fetched_Data = await fetch(url);
-      const json = await fetched_Data.json();
-      setData(json);
-      console.log("Fetched data!"); 
-      console.log(json);
-    }
     
-
-    fetchData();
     try {
       
     } catch (error) {
@@ -26,23 +26,50 @@ export default function App() {
     
     },[]);
 
-
-    
-
-
-  
   return (
     <>
+    <h1>Data Request and Response!!!</h1>
+    {mydata && <ul>{mydata.map(
+    (d=><li key={d.id}>{d.s1} {d.s2}</li>)
+    )}</ul>}
+    <button 
+    onClick={()=> setMyData([{id:0,s1:"Hello this is the mydata"},{id:1,s2:"I am the next data piece!"}])}
+    >
+    Add
+    </button>
+
+    {isLoading && <div>Loading...</div>}
+     {data && <div>The data passed in the data state!!!</div>}
+
      <ul>
-      {data.map((d)=>(
-        <li key={d.id}>{d.title}</li>
-        
-      ))}
-    </ul>
+      {todosdata && todosdata.map(
+        (td=><li key={td.id}>{td.id} {td.title} {JSON.stringify(td.completed)}</li>)
+      )}
+      </ul>
+
+      <button 
+    onClick={()=> fetchData()}
+    >
+    FetchData
+    </button>
       
     </>
   );
 }
+
+export default App;
+
+
+
+
+
+
+
+  
+
+  
+  
+
 
 
 
